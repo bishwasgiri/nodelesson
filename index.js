5e6ebe464c5d7dc776e758b486cf2f95cce10d10
bishwasgiri <giribishwas4@gmail.com>
2022-08-15 11:51:48 +0545

    2.File modules

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
+const homePage = fs.readFileSync('index.html');
+const aboutPage = fs.readFileSync('about.html');
+const contactPage = fs.readFileSync('contact.html');
+const errorPage = fs.readFileSync('error.html');
+
+// request handler function
 const server = http.createServer((req,res)=>{
-    // console.log(req.url);
-    // res.end('hello node js');
+
     if(req.url ==='/about'){
-        res.end('the about page')
+        // res.end('the about page')
+        res.end(aboutPage);
     }else if(req.url ==='/contact'){
-        res.end('the contact page')
+        // res.end('the contact page')
+        res.end(contactPage)
     }else if(req.url ==='/'){
-        res.end('the home page')
+        // res.end('the home page')
+        res.end(homePage);
     }else {
         res.writeHead(404)
-        res.end('page not found');
+        // res.end('page not found');
+       res.end(errorPage); 
     }
 })
 
