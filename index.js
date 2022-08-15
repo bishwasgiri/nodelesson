e0077b7c1b03ae6504cc0931a2b16fef9b2333cd
bishwasgiri <giribishwas4@gmail.com>
2022-08-15 11:50:37 +0545

    2.filemodules

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url);
-    res.end('hello node js');
+    // console.log(req.url);
+    // res.end('hello node js');
+    if(req.url ==='/about'){
+        res.end('the about page')
+    }else if(req.url ==='/contact'){
+        res.end('the contact page')
+    }else if(req.url ==='/'){
+        res.end('the home page')
+    }else {
+        res.writeHead(404)
+        res.end('page not found');
+    }
 })
 
 server.listen(3000);
